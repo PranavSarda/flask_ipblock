9bd6753d4f68cc80ca42a796484e72102edcd5db
anivedh555 <anivedh.21810720@viit.ac.in>
2020-08-01 20:09:52 +0530

    Blacklisted IP list working

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import time
 import sys
 app = Flask(__name__)
 
-
-usrname='ajaj'
+blacklisted_ip=[]
+usrname='ajaj' #Take this as form input (hard coded for now)
 passwd='heyhey'
-x={}
-y={}
+x={} # Stores the fields for the GET requests
+y={} # Stores the fields for the POST requests
 req=3 #maximum number of requests allowed per user
 @app.route('/', methods=['GET','POST'])
 def get_tasks():
@@ -16,16 +16,20 @@ def get_tasks():
         print(request.remote_addr)
         global req
         while(req>0):
-            req-=1
+           req-=1
             if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
                 x={'ip': request.environ['REMOTE_ADDR'],'username':usrname,'passwd':passwd, 'timestamp':time.time()}
-                print(x)
-                print(req)
+                #print(x)
+                #print(req)
                 return jsonify(x),200
             else:
                 y={'ip':request.environ['HTTP_X_FORWARDED_FOR']}
                 return jsonify({'ip': request.environ['HTTP_X_FORWARDED_FOR']}), 200
         else:
+            t=request.environ['REMOTE_ADDR']
+            if t not in blacklisted_ip:
+                blacklisted_ip.append(t)
+            print(blacklisted_ip)
             return sys.exit()
     elif request.method=='POST':
         while(req>0):
